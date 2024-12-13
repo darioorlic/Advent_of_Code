90e79ff4576a052f43ec62a8578a6236eecbf5e4
Dario Orlic <dario.orlic3@gmail.com>
2024-12-13 20:27:53 +0100

    Day 6, part 2: modified get_map() to scan all obstacles in a cross from the starting obstacle (updating the map now only requires the position of the tmp_obstacle).

--- 2024/06.py
+++ 2024/06.py
@@ -4,7 +4,6 @@ t0 = perf_counter()
 
 with open('06_in.txt', 'r') as f:
     input = tuple(tuple(el) for el in f.read().splitlines())
-# print(input)
 
 maxI = len(input)
 maxJ = len(input[0])
@@ -44,48 +43,56 @@ while i in range(1, maxI - 1) and j in range(1, maxJ - 1):
     path.append((i, j))
 
 print('First answer:', len(set(path)))
-# path.discard(init_pos)
 t1 = perf_counter()
 print(f'Time: {t1-t0:.3}s')
 
 # Second part #######
 
-def get_map(obstacle, all_obstacles):
+def get_map(obstacle, obstacles, map, update = False):
     out = dict()
     for pos_num in range(4):
-        pos = (obstacle[0] + directions[pos_num][0], obstacle[1] + directions[pos_num][1])
-        # if pos[0] in range(1, maxI - 1) and pos[1] in range(1, maxJ - 1) and pos not in all_obstacles:
+        pos1 = (obstacle[0] + directions[pos_num][0], obstacle[1] + directions[pos_num][1])
+        if update or pos1 not in map:
 
-        dir_num = (pos_num - 1) % 4
-        direction = directions[dir_num]
-        i, j = pos
-        while True:
-            ni, nj = i + direction[0], j + direction[1]
-            if (ni, nj) in all_obstacles:
-                jump = i, j
-                break
-            i, j = ni, nj
-            if not (i in range(1, maxI - 1) and j in range(1, maxJ - 1)): 
-                jump = 'E'
-                break
-        if jump != pos:
-            out.update({pos:jump})
+            dir_num = (pos_num - 1) % 4
+            direction = directions[dir_num]
+            direction_left = directions[(dir_num - 1) % 4]
+            i, j = pos1
+            pos2 = i + direction[0] + direction_left[0], j + direction[1] + direction_left[1]
+
+            encountered = False
+            encountered_left = False
+            while True:
+                ni, nj = i + direction[0], j + direction[1]
+                if not (ni in range(maxI) and nj in range(maxJ)): 
+                    if not encountered: out.update({pos1:'E'})
+                    break
+                li, lj = ni + direction_left[0], nj + direction_left[1]
+                if (li, lj) in obstacles: encountered_left = True
+                if (ni, nj) in obstacles:
+                    if (i, j) != pos1:
+                        if not encountered:
+                            out.update({pos1:(i, j)})
+                        if not encountered_left:
+                            out.update({(li, lj): pos2})
+                    encountered = True
+                if encountered and encountered_left: break
+                i, j = ni, nj
     return(out)
 
-def in_loop(i, j, map):
+def in_loop(pos, map):
     visited = set()
     while True:
-        # print(i, j)
-        if (i, j) in visited: return(1)
-        if map[(i, j)] == 'E': return(0)
-        visited.add((i, j))
-        i, j = map[(i, j)]
+        if pos in visited: return(1)
+        if map[pos] == 'E': return(0)
+        visited.add(pos)
+        pos = map[pos]
 
 
 # Initial map
 init_map = dict()
 for obstacle in fixed_obstacles:
-    init_map.update(get_map(obstacle, fixed_obstacles))
+    init_map.update(get_map(obstacle, fixed_obstacles, init_map))
 
 # Go through all new obstacles on the path
 ans2 = 0
@@ -93,31 +100,15 @@ direction = directions[init_dir_num]
 visited = set([init_pos])
 for tmp_obstacle, starting_pos in zip(path[1:], path[:-1]):
     if tmp_obstacle in visited: continue
-    visited.add(starting_pos)
-    # print(tmp_obstacle)
+
+    visited.add(tmp_obstacle)
     all_obstacles = fixed_obstacles.copy()
     all_obstacles.add(tmp_obstacle)
-    map = init_map.copy()
-
-    # Which obstacles' maps need to be updated
-    to_update = set()
-    to_update.add(tmp_obstacle)
-    for dir_num in range(4):
-        i, j = tmp_obstacle[0] + directions[dir_num][0], tmp_obstacle[1] + directions[dir_num][1]
-        while i in range(maxI) and j in range(maxJ):
-            if (i, j) in all_obstacles: break
-            ni, nj = i + directions[(dir_num + 1) % 4][0], j + directions[(dir_num + 1) % 4][1]
-            if (ni, nj) in all_obstacles: to_update.add((ni, nj))
-            i, j = i + directions[dir_num][0], j + directions[dir_num][1]
-    # print(to_update)
 
     map = init_map.copy()
-    for obstacle in to_update:
-        map.update(get_map(obstacle, all_obstacles))
-    # print(map)
+    map.update(get_map(tmp_obstacle, all_obstacles, map, True))
 
-    i, j = starting_pos
-    ans2 += in_loop(i, j, map)
+    ans2 += in_loop(starting_pos, map)
 
 print('Second answer:', ans2)
 t2 = perf_counter()
