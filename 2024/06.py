e9a9962a0942aa92d9640ccb91d022bd536293b4
Dario Orlic <dario.orlic3@gmail.com>
2024-12-13 15:52:53 +0100

    Replaced 06.py with 06_map.py

--- 2024/06.py
+++ 2024/06.py
@@ -1,16 +1,23 @@
 from time import perf_counter
-from copy import deepcopy
 
 t0 = perf_counter()
 
 with open('06_in.txt', 'r') as f:
-    input = f.read().splitlines()
-input = [list(el) for el in input]
+    input = tuple(tuple(el) for el in f.read().splitlines())
 # print(input)
 
-i, j = [[i, j] for i, row in enumerate(input) for j, el in enumerate(row) if el in ('^', '>', 'v', '<')][0]
-init_pos = [i, j]
+maxI = len(input)
+maxJ = len(input[0])
 
+fixed_obstacles = set()
+for i in range(maxI):
+    for j in range(maxJ):
+        if input[i][j] in ('^', '>', 'v', '<'):
+            init_pos = (i, j)
+        if input[i][j] == '#':
+            fixed_obstacles.add((i, j))
+
+i, j = init_pos
 if input[i][j] == '^':
     init_dir_num = 0
 elif input[i][j] == '>':
@@ -21,76 +28,100 @@ elif input[i][j] == '<':
     init_dir_num = 3
 
 directions = ((-1,0), (0,1), (1,0), (0,-1)) # up, right, down, left
-direction = directions[init_dir_num % 4]
 
-obstacles = tuple()
+# First part #######
+path = set()
 dir_num = init_dir_num
-data = deepcopy(input)
-while 1 <= i < len(data) - 1 and 1 <= j < len(data[0]) - 1:
-    data[i][j] = 'X'
-    if data[i + direction[0]][j + direction[1]] == '#':
+direction = directions[dir_num % 4]
+path.add((i, j))
+while i in range(1, maxI - 1) and j in range(1, maxJ - 1):
+    ni, nj = i + direction[0], j + direction[1]
+    if input[ni][nj] == '#': 
         dir_num += 1
         direction = directions[dir_num % 4]
-        if data[i + direction[0]][j + direction[1]] == '#':
-            dir_num += 1
-            direction = directions[dir_num % 4]
-    else:
-        data[i][j] = 'X'
-        i += direction[0]
-        j += direction[1]
-        if not bool(obstacles.count((i,j))): obstacles += ((i, j),)
+        continue
+    i, j = ni, nj
+    path.add((i, j))
 
-ans1 = sum(el.count('X') for el in data) + 1
-print('First answer:', ans1)
-obstacles = obstacles[1:]
-# print(obstacles)
+print('First answer:', len(path))
+path.discard(init_pos)
+t1 = perf_counter()
+print(f'Time: {t1-t0:.3}s')
 
-fixed_obstacles = tuple((i, j) for i, row in enumerate(input) for j, el in enumerate(row) if el == '#')
-# print(fixed_obstacles)
+# Second part #######
 
-max_hit = 5
-max_iteration = 10**10
+def get_map(obstacle, all_obstacles):
+    out = dict()
+    for pos_num in range(4):
+        pos = (obstacle[0] + directions[pos_num][0], obstacle[1] + directions[pos_num][1])
+        # if pos[0] in range(1, maxI - 1) and pos[1] in range(1, maxJ - 1) and pos not in all_obstacles:
+
+        dir_num = (pos_num - 1) % 4
+        direction = directions[dir_num]
+        i, j = pos
+        while True:
+            ni, nj = i + direction[0], j + direction[1]
+            if (ni, nj) in all_obstacles:
+                jump = i, j
+                break
+            i, j = ni, nj
+            if not (i in range(1, maxI - 1) and j in range(1, maxJ - 1)): 
+                jump = 'E'
+                break
+        if jump != pos:
+            out.update({pos:jump})
+    return(out)
+
+def in_loop(i, j, map):
+    visited = set()
+    while True:
+        # print(i, j)
+        if (i, j) in visited: return(1)
+        if map[(i, j)] == 'E': return(0)
+        visited.add((i, j))
+        i, j = map[(i, j)]
+
+
+# Initial map
+init_map = dict()
+for obstacle in fixed_obstacles:
+    init_map.update(get_map(obstacle, fixed_obstacles))
+
+# Go through all new obstacles on the path
 ans2 = 0
+direction = directions[init_dir_num]
+for tmp_obstacle in path:
+    # print(tmp_obstacle)
+    all_obstacles = fixed_obstacles.copy()
+    all_obstacles.add(tmp_obstacle)
+    map = init_map.copy()
 
-for obstacle in obstacles:
-    iteration = 0
-    data = deepcopy(input)
-    dir_num = init_dir_num
-    direction = directions[dir_num % 4]
-    if data[obstacle[0]][obstacle[1]] not in ('^', '>', 'v', '<'):
-        data[obstacle[0]][obstacle[1]] = '#'
-    # hit = 0
-    hits = [0] * (len(fixed_obstacles) + 1)
+    # Which obstacles' maps need to be updated
+    to_update = set()
+    to_update.add(tmp_obstacle)
+    for dir_num in range(4):
+        i, j = tmp_obstacle[0] + directions[dir_num][0], tmp_obstacle[1] + directions[dir_num][1]
+        while i in range(maxI) and j in range(maxJ):
+            if (i, j) in all_obstacles: break
+            ni, nj = i + directions[(dir_num + 1) % 4][0], j + directions[(dir_num + 1) % 4][1]
+            if (ni, nj) in all_obstacles: to_update.add((ni, nj))
+            i, j = i + directions[dir_num][0], j + directions[dir_num][1]
+    # print(to_update)
 
-    # print()
-    # for el in data:
-    #     print(el)
+    map = init_map.copy()
+    for obstacle in to_update:
+        map.update(get_map(obstacle, all_obstacles))
+    # print(map)
 
+    # Walk to first obstacle
     i, j = init_pos
-    while 1 <= i < len(data) - 1 and 1 <= j < len(data[0]) - 1 and max(hits) < max_hit and iteration < max_iteration:
-        if data[i + direction[0]][j + direction[1]] == '#':
-            if (i + direction[0], j + direction[1]) == obstacle:
-                hits[-1] += 1
-            if bool(fixed_obstacles.count((i + direction[0], j + direction[1]))):
-                hits[fixed_obstacles.index((i + direction[0], j + direction[1]))] += 1
-            dir_num += 1
-            direction = directions[dir_num % 4]
-            if data[i + direction[0]][j + direction[1]] == '#':
-                if (i + direction[0], j + direction[1]) == obstacle:
-                    hits[-1] += 1
-                if bool(fixed_obstacles.count((i + direction[0], j + direction[1]))):
-                    hits[fixed_obstacles.index((i + direction[0], j + direction[1]))] += 1
-                dir_num += 1
-                direction = directions[dir_num % 4]
-        else:
-            i += direction[0]
-            j += direction[1]
-        iteration += 1
+    while True:
+        ni, nj = i + direction[0], j + direction[1]
+        if (ni, nj) in all_obstacles: break
+        i, j = ni, nj
     
-    # print(hits)
-    if max(hits) == max_hit or iteration == max_iteration: ans2 += 1
+    ans2 += in_loop(i, j, map)
 
 print('Second answer:', ans2)
-
-t1 = perf_counter()
-print(f'Time: {t1-t0:.2}s')
+t2 = perf_counter()
+print(f'Time: {t2-t0:.3}s')
