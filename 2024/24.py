6b0e2e19c73db4a0703ec628611033a627be5784
Dario Orlic <dario.orlic3@gmail.com>
2025-01-06 00:00:32 +0100

    Day 24: the final star

--- 2024/24.py
+++ 2024/24.py
@@ -1,5 +1,4 @@
 from functools import cache
-from copy import deepcopy
 
 with open('24_in.txt') as f:
     input = f.read().splitlines()
@@ -16,7 +15,6 @@ init_gates = []
 while i < len(input):
     init_gates.append([el for el in input[i].split(' ') if el != '->'])
     i += 1
-# print(values, gates)
 
 @cache
 def solve_gate(in1, operator, in2, out):
@@ -39,57 +37,82 @@ def calculate_values(gates):
 values = calculate_values(init_gates)
 
 z_values = dict((key,val) for key, val in sorted(values.items()) if key[0] == 'z')
-print(z_values)
 ans1 = str()
 for el in z_values.values():
     ans1 = str(el) + ans1
 ans1 = int(ans1, 2)
-# print('Part one:', ans1)
+print('Part one:', ans1)
 
 # Part two #####################
 
-# Get the desired value of z from x and y
-x_values = dict((key,val) for key, val in sorted(values.items()) if key[0] == 'x')
-y_values = dict((key,val) for key, val in sorted(values.items()) if key[0] == 'y')
-x_val = str()
-y_val = str()
-target = str()
-for val1, val2 in zip(x_values.values(), y_values.values()):
-    x_val = str(val1) + x_val
-    y_val = str(val2) + y_val
-    target = str(val1 & val2) + target
-x_val = int(x_val, 2)
-y_val = int(y_val, 2)
-# target = x_val + y_val
-# target = f'{target:0{len(z_values)}b}'
-# print(target)
-z_tagets = dict(('z' + f'{i:02}', int(val)) for i, val in enumerate(reversed(target)))
-print(z_tagets)
+lefts = []
+rights = []
+for gate in init_gates:
+    lefts.append(set(gate[:3]))
+    rights.append(gate[3])
+gates = [set(row) for row in init_gates]
 
-# Compare the obtained value of z with the target value of z
-def get_score(values):
-    score = 0
-    for key, val in z_tagets.items():
-        if val == values[key]: score += 1
-        # else: print(key)
-    return(score)
-print(get_score(values))
+to_swap = []
 
-def swap_outputs(init_gates, score, init_swapped = set(), depth = 0):
-    if depth == 2: return
-    for i in range(len(init_gates)):
-        if i in init_swapped: continue
-        for j in range(i+1, len(init_gates)):
-            if j in init_swapped: continue
-            swapped = init_swapped.copy()
-            gates = deepcopy(init_gates)
-            gates[i][3], gates[j][3] = gates[j][3], gates[i][3]
-            swapped = swapped.union({i,j})
+# First bit
+if {'x00', 'XOR', 'y00', 'z00'} not in gates: 
+    to_swap.append('z00')
+    to_swap.append(rights[lefts.index({'x00', 'XOR', 'y00'})])
+d = (rights[lefts.index({'x00', 'AND', 'y00'})])
 
-            new_score = get_score(calculate_values(gates))
-            if new_score == len(z_tagets): return(swapped)
-            if new_score > score+1:
-                out = swap_outputs(gates, new_score, swapped, depth + 1)
-    return(out)
-            
-print(swap_outputs(init_gates, get_score(values)))
+# Middle bits
+for i in range(1, int(len(init_values) / 2)):
+    x = f'x{i:02}'
+    y = f'y{i:02}'
+    z = f'z{i:02}'
+
+    # A
+    a = (rights[lefts.index({x, 'XOR', y})]) 
+
+    # Z
+    if {a, 'XOR', d} in lefts:
+        if rights[lefts.index({a, 'XOR', d})] != z:
+            to_swap.append(z)
+            to_swap.append(rights[lefts.index({a, 'XOR', d})])
+
+    # B
+    if {a, 'AND', d} in lefts:
+        b = (rights[lefts.index({a, 'AND', d})])
+    else:
+        for left, right in zip(lefts, rights):
+            if {a, 'AND'}.issubset(left):
+                to_swap.append(d)
+                to_swap.extend(left.difference({a, 'AND'}))
+                b = (right)
+                break
+            if {'AND', d}.issubset(left):
+                to_swap.append(a)
+                to_swap.extend(left.difference({d, 'AND'}))
+                b = (right)
+                break
+    
+    # C
+    c = (rights[lefts.index({x, 'AND', y})]) 
+
+    # D
+    if {b, 'OR', c} in lefts:
+        d = (rights[lefts.index({b, 'OR', c})])
+    else:
+        for left, right in zip(lefts, rights):
+            if {b, 'OR'}.issubset(left):
+                to_swap.append(c)
+                to_swap.extend(left.difference({b, 'OR'}))
+                d = (right)
+                break
+            if {'OR', c}.issubset(left):
+                to_swap.append(b)
+                to_swap.extend(left.difference({c, 'OR'}))
+                d = (right)
+                break
+    
+# Last bit
+z = f'z{int(len(init_values) / 2):02}'
+if d != z:
+    to_swap.extend([d, z])
+
+print('Part two:', ','.join(sorted(set(to_swap))))
