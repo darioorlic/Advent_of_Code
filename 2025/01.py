e476e9b5319566f8a1421fc843062234e5964830
Dario Orlic <dario.orlic3@gmail.com>
2025-12-01 21:11:28 +0100

    Day 1: cleaned up

--- 2025/01.py
+++ 2025/01.py
@@ -1,21 +1,17 @@
-from math import floor
 with open('2025/01_in.txt') as f:
     inp = f.readlines()
-
-moves = []
-for line in inp:
-    if line[0] == 'L': moves.append(- int(line[1:]))
-    else: moves.append(int(line[1:]))
 
 pos = 50
 out1 = 0
 out2 = 0
-for move in moves:
-    on_zero = False
-    next_num = pos + move
-    next_pos = (next_num) % 100
+for line in inp:
+    if line[0] == 'L': move = - int(line[1:])
+    else: move = int(line[1:])
 
-    passes = floor(next_num / 100)
+    next_num = pos + move
+    next_pos = next_num % 100
+
+    passes = next_num // 100
     out2 += abs(passes)
     if passes <= -1:
         if pos == 0 and next_pos != 0: out2 -= 1
