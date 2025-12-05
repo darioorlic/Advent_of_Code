80107d2447c5903f793382f88d5c99f014da3b12
Dario Orlic <dario.orlic3@gmail.com>
2025-12-05 15:19:12 +0100

    Day 5

---
+++ 2025/05.py
@@ -0,0 +1,34 @@
+with open('2025/05_in.txt') as f:
+    inp = f.read()
+inp = inp.splitlines()
+
+section = 0
+ranges = []
+ans1 = 0
+for line in inp:
+    if not line:
+        section = 1
+        continue
+    if section == 0:
+        start, end = line.split('-')
+        ranges.append((int(start), int(end)+1))
+    else:
+        id = int(line)
+        for i, (start, end) in enumerate(ranges):
+            if start <= id < end:
+                ans1 += 1
+                break
+print('Part 1:', ans1)
+
+ans2 = 0
+ranges.sort()
+lower, upper = ranges[0]
+for start, end in ranges[1:]:
+    if start <= upper:
+        upper = max(end, upper)
+    else:
+        ans2 += upper - lower
+        lower, upper = start, end
+ans2 += upper - lower
+
+print('Part 2:', ans2)
